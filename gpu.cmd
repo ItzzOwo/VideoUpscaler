84381704804cf137f4e9affd3e00fed14cf5589c
Reece <38808457+ItzzOwo@users.noreply.github.com>
2022-09-09 23:33:06 +0100

    Fixed errors
    1 - Fixed the incorrect output file name
    2 - Cleaned code

--- gpu.cmd
+++ gpu.cmd
@@ -5,8 +5,8 @@ if exist %cd%\in.mp4.mp4 (ren %cd%\in.mp4.mp4 in.mp4) else (goto conversion)
 if exist C:\ffmpeg\bin\ffmpeg.exe (goto conversion) else (goto install)
 :conversion
 title Upscaling your video... 
-C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_amf -profile:v high -preset fast -qmin 13 -qmax 13 -c:a copy "output_file.mp4" 
-C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_nvenc -profile:v high -preset fast -rc constqp -qp 14 -c:a copy "output_file.mp4" -y
+C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_amf -profile:v high -preset fast -qmin 13 -qmax 13 -c:a copy "outmp4" 
+C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_nvenc -profile:v high -preset fast -rc constqp -qp 14 -c:a copy "out.mp4" -y
 goto success
 :installFFmpeg
 title Installing FFmpeg...
@@ -15,12 +15,12 @@ powershell Invoke-WebRequest "https://cdn.discordapp.com/attachments/98566793096
 goto start
 :completed
 SET msgboxTitle=Successfully Completed!
-SET msgboxBody=The file is named "out.mp4". Have fun with the 4k! 
+SET msgboxBody=The file is named "out.mp4" - Subscribe to ItzOwo on YouTube
 SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
 IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
 ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
 WSCRIPT "%tmpmsgbox%"
 
-Subscribe to ItzOwo BTW :D
+
 
  
