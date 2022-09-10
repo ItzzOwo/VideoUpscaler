d10ad6e762af0c629b849f615b4a8ee92c2ec798
Reece <38808457+ItzzOwo@users.noreply.github.com>
2022-09-10 15:09:59 +0100

    Update gpu.cmd

--- gpu.cmd
+++ gpu.cmd
@@ -5,7 +5,7 @@ if exist %cd%\in.mp4.mp4 (ren %cd%\in.mp4.mp4 in.mp4) else (goto conversion)
 if exist C:\ffmpeg\bin\ffmpeg.exe (goto conversion) else (goto install)
 :conversion
 title Upscaling your video... 
-C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_amf -profile:v high -preset fast -qmin 13 -qmax 13 -c:a copy "outmp4" 
+C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_amf -profile:v high -preset fast -qmin 13 -qmax 13 -c:a copy "out.mp4" 
 C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_nvenc -profile:v high -preset fast -rc constqp -qp 14 -c:a copy "out.mp4" -y
 goto success
 :installFFmpeg
