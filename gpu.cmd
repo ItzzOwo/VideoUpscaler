d502843e3e84c429375c6538258bde4777028449
Reece <38808457+ItzzOwo@users.noreply.github.com>
2022-11-14 16:11:04 +0000

    Update gpu.cmd

--- gpu.cmd
+++ gpu.cmd
@@ -1,10 +1,9 @@
 @echo off
 :input
-if exist %cd%\in.mp4.mp4 (ren %cd%\in.mp4.mp4 in.mp4) else (goto conversion)
+if exist %cd%\in.mp4 (ren %cd%\in.mp4 in.mp4) else (goto conversion)
 :start
 if exist C:\ffmpeg\bin\ffmpeg.exe (goto conversion) else (goto install)
 :conversion
-title Upscaling your video... 
 C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_amf -profile:v high -preset fast -qmin 13 -qmax 13 -c:a copy "out.mp4" 
 C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_nvenc -profile:v high -preset fast -rc constqp -qp 14 -c:a copy "out.mp4" -y
 goto success
@@ -14,13 +13,6 @@ powershell Invoke-WebRequest "https://cdn.discordapp.com/attachments/98566793096
 %temp%\ffmpeg-4.2-setup.exe
 goto start
 :completed
-SET msgboxTitle=Successfully Completed!
-SET msgboxBody=The file is named out.mp4 - Subscribe to ItzOwo on YouTube
-SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
-IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
-ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
-WSCRIPT "%tmpmsgbox%"
-
 
 
  
