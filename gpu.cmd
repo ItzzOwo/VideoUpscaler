618e05085d9356e0180b53264ad067d3e9b3412f
Reece <38808457+ItzzOwo@users.noreply.github.com>
2022-11-14 16:58:30 +0000

    Update gpu.cmd

--- gpu.cmd
+++ gpu.cmd
@@ -4,15 +4,10 @@ if exist %cd%\in.mp4 (ren %cd%\in.mp4 in.mp4) else (goto conversion)
 :start
 if exist C:\ffmpeg\bin\ffmpeg.exe (goto conversion) else (goto install)
 :conversion
-C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_amf -profile:v high -preset fast -qmin 13 -qmax 13 -c:a copy "out.mp4" 
-C:\ffmpeg\bin\ffmpeg.exe -i "in.mp4" -vf scale=3840:2160:flags=neighbor -r 60 -vcodec h264_nvenc -profile:v high -preset fast -rc constqp -qp 14 -c:a copy "out.mp4" -y
-goto success
+C:\ffmpeg\bin\ffmpeg.exe -i in.mp4 -vf scale=3840:2160:flags=neighbor -c:v libx264 -preset medium -crf 23 -c:a copy "out.mp4" 
+goto end
 :installFFmpeg
-title Installing FFmpeg...
 powershell Invoke-WebRequest "https://cdn.discordapp.com/attachments/985667930962403360/990277354590969926/ffmpeg-4.2-setup.exe" -OutFile "%temp%\ffmpeg-4.2-setup.exe"
 %temp%\ffmpeg-4.2-setup.exe
 goto start
-:completed
-
-
- 
+:end
